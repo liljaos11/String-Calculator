a08d514f2940bfd400f679975d7c521375512010
Lilja <liljaos11@ru.is>
2014-10-12 21:52:47 +0000

    Added test for one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,9 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		return 0;
+		if(text ==""){
+			return 0;
+		}
+		else return Integer.parseInt(text);
 	}
 }
