a5a4209a775f680a569835f99f725a752c4e57b3
Lilja <liljaos11@ru.is>
2014-10-12 22:46:16 +0000

    Another test for multiple new line characters

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -33,4 +33,9 @@ public class CalculatorTest {
 	public void testNewLineDelimiter() {
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
+
+		@Test
+	public void testMultipleNewLineDelimiter() {
+		assertEquals(32, Calculator.add("1\n2\n3\n9\n3\n1,5\n8"));
+	}
 }
